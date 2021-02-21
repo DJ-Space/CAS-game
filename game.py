113949b11e2ddf19d861b79a40851a2e32ca3954
Daniel Strader <spacetimedj@gmail.com>
2021-02-21 16:10:42 -0500

    added physics

--- game.py
+++ game.py
@@ -2,17 +2,20 @@
 Platformer Game
 """
 import arcade
-import random
 
 # Constants
-SCREEN_WIDTH = 1500
-SCREEN_HEIGHT = 975
-SCREEN_TITLE = "CAS-Game"
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
 
 # Constants used to scale our sprites from their original size
 CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 COIN_SCALING = 0.5
+
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 5
+
 
 class MyGame(arcade.Window):
     """
@@ -22,8 +25,8 @@ class MyGame(arcade.Window):
     def __init__(self):
 
         # Call the parent class and set up the window
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable = True)
-        
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
         # These are 'lists' that keep track of our sprites. Each sprite should
         # go into a list.
         self.coin_list = None
@@ -32,11 +35,12 @@ class MyGame(arcade.Window):
 
         # Separate variable that holds the player sprite
         self.player_sprite = None
-        
-        bgcolor = [arcade.csscolor.DARK_OLIVE_GREEN, arcade.csscolor.SLATE_GRAY, arcade.csscolor.ROYAL_BLUE]
-        windowcolor = random.choice(bgcolor)
-        arcade.set_background_color(windowcolor)
-        
+
+        # Our physics engine
+        self.physics_engine = None
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
         # Create the Sprite lists
@@ -45,7 +49,7 @@ class MyGame(arcade.Window):
         self.coin_list = arcade.SpriteList(use_spatial_hash=True)
 
         # Set up the player, specifically placing it at these coordinates.
-        image_source = ":resources:images/animated_characters/robot/robot_idle.png"
+        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
@@ -67,21 +71,53 @@ class MyGame(arcade.Window):
 
         for coordinate in coordinate_list:
             # Add a crate on the ground
-            wall = arcade.Sprite(":resources:images/tiles/brickBrown.png", TILE_SCALING)
+            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
             wall.position = coordinate
             self.wall_list.append(wall)
-                   
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
     def on_draw(self):
         """ Render the screen. """
 
+        # Clear the screen to the background color
         arcade.start_render()
-        # Code to draw the screen goes here
-        
+
         # Draw our sprites
         self.wall_list.draw()
         self.coin_list.draw()
         self.player_list.draw()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
 
 
 def main():
@@ -93,3 +129,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
